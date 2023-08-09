658a7c4708be12e0d530c2d383f4ed8b93785d72
Daniel <126919570+Lux00000@users.noreply.github.com>
2023-08-09 13:21:46 +0300

    Add files via upload

---
+++ Linked_List.py
@@ -0,0 +1,93 @@
+class LinkedList:
+    head = None
+    lenght = 0
+
+    class Node:
+        element = None
+        next_node = None
+
+        def __init__(self, element, next_node = None):
+            self.element = element
+            self.next_node = next_node
+
+    def append(self, element):
+        if not self.head:
+            self.head = self.Node(element)
+            self.lenght += 1 
+            return element
+        
+        node = self.head
+        while node.next_node:
+            node = node.next_node
+
+        node.next_node = self.Node(element)
+        self.lenght += 1
+        return element
+    
+    def __str__(self):
+        node = self.head
+        line = "["
+        while node.next_node:
+            line += str(node.element) + ", "
+            node = node.next_node
+        line += str(node.element) + "]"
+        
+        return line
+    
+    def getitem(self,key):
+        i = 0
+        node = self.head
+
+        while i < key:
+            node = node.next_node
+            i += 1
+        return node.element
+    
+    def insert(self, key, value):
+        i = 0
+        node = self.head
+        prev_node = self.head
+
+        if key == 0:
+            old_head = self.head
+            self.head = self.Node(value, next_node= old_head)
+            self.lenght += 1
+            return value
+        
+        while i < key:
+            prev_node = node
+            node = node.next_node
+            i += 1
+        
+        prev_node.next_node = self.Node(value, next_node= node)
+        self.lenght += 1
+        return value
+    
+    def delitem(self, key):
+        i = 0 
+        node = self.head
+        prev_node = node
+
+        if key == 0:
+            old_head = self.head
+            element = self.head.element
+            self.head = self.head.next_node
+            self.lenght -= 1
+
+            del old_head
+            return element
+        
+        while i < key:
+            prev_node = node
+            node = node.next_node
+            i += 1
+
+            prev_node.next_node = node.next_node
+            element = node.element
+            self.lenght -= 1
+
+            del node
+            return element
+
+
+        
